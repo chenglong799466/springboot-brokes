34bef3656af55cbc8c4a9617f9750393a0289642
chenglong <799466827@qq.com>
2018-04-02 14:35:34 +0800

    test

--- src/main/java/com/example/demo/service/HelloService.java
+++ src/main/java/com/example/demo/service/HelloService.java
@@ -22,7 +22,7 @@ public class HelloService {
      * cron 元素可以是一个值（6），一个连续区间（9-12），一个间隔时间（8-18/4 表示每隔4小时）
      * 一个列表（1,2,3），通配符。
      */
-    //@Scheduled(fixedDelay = 5000)
+    // @Scheduled(fixedDelay = 5000)
     //@Scheduled(cron = "0 * * * * ?")
     public void schedule() {
         DateTime nowTime = new DateTime();
